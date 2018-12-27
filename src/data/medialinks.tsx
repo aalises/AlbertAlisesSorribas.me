528d9006e9af78fb8667c48e1e2050f541a36615
Albert Alises <albert.alises@gmail.com>
2018-12-27 13:38:59 +0100

    Adjust header and media links

--- src/data/medialinks.tsx
+++ src/data/medialinks.tsx
@@ -1,8 +1,8 @@
 const mediaLinks = [
-    {link:"mailto:albert.alises@gmail.com" , icon: "fas fa-lg fa-envelope"},
-    {link:"https://www.linkedin.com/in/albert-alises-sorribas-649176ba/" , icon: "fab fa-lg fa-linkedin" },
-    {link: "https://github.com/aalises" , icon: "fab fa-lg fa-github" },
-    {link: "https://open.spotify.com/user/ironicpsycho?si=jn5PWC-xQxCDLEmiaxCHRw", icon: "fab fa-lg fa-spotify" }
+    {link:"mailto:albert.alises@gmail.com" , icon: "fas fa-2x fa-envelope"},
+    {link:"https://www.linkedin.com/in/albert-alises-sorribas-649176ba/" , icon: "fab fa-2x fa-linkedin" },
+    {link: "https://github.com/aalises" , icon: "fab fa-2x fa-github" },
+    {link: "https://open.spotify.com/user/ironicpsycho?si=jn5PWC-xQxCDLEmiaxCHRw", icon: "fab fa-2x fa-spotify" }
 ]
 
 export default mediaLinks;
--- src/sections/medialinks/MediaLinks.tsx
+++ src/sections/medialinks/MediaLinks.tsx
@@ -6,7 +6,7 @@ export default () => {
         <div style={{justifyContent: "center"}} className="field is-grouped is-grouped-multiline">
         {MediaLinks.map(el => {
           return (
-            <a key={`${el.icon}-medialink`} href={el.link} target="_blank" className="icon is-medium media-link">
+            <a style={{margin: '0rem 0.5rem'}} key={`${el.icon}-medialink`} href={el.link} target="_blank" className="icon is-medium media-link">
               <i className={`${el.icon}`}></i>
             </a>
           )
