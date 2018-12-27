1f7c8742771137495684eb8238952170ceae98a7
Albert Alises <albert.alises@gmail.com>
2018-12-27 16:07:09 +0100

    Add position fixed instead of sticky

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import SidebarData from "../data/sidebar";
 
 export default () => {
     return (
-    <aside style={{position: 'sticky', top: '30%'}} className="menu is-hidden-touch has-text-centered">
+    <aside style={{position: 'fixed', top: '30%'}} className="menu is-hidden-touch has-text-centered">
         <ul className="menu-list">
         {SidebarData.map(el => {
             return <li key={`sidebar-${el}`}onClick={() => window.scroll({top: document.getElementById(`section-${el}`).offsetTop, behavior: 'smooth' })}><a>{el}</a></li>
