d38d149ab3396d620e9020ca699b06358a991fbf
Albert Alises <albert.alises@gmail.com>
2019-02-26 15:42:43 +0100

    Fix path for favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
         display: 'standalone',
         icons: [
           {
-            src: '/static/favicon.ico',
+            src: 'favicon.ico',
             sizes: '120x120',
             type: 'image/ico',
           },
