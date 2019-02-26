28c8d132fc29783bbdd4024533822d260ff94b71
Albert Alises <albert.alises@gmail.com>
2019-02-26 12:34:52 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4e236d1c311683ddd57c.js"
+    "url": "webpack-runtime-e6d0586ea9d122e99491.js"
   },
   {
     "url": "app-6912501a70090b9d4618.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c2b0ef8d948208345517f77f2370ea53"
+    "revision": "0c9822fa8b4eaaceff4f24636061b3ab"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
